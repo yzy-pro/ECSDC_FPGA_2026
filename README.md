a86a6dc63c8ba0ebaf32f46c2d7e61e84be7f914
Your Name <you@example.com>
2026-09-25 16:43:10 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 pds工具安装路径：E:\work\pango\PDS_2025.2-ads\bin
+
+33 EX_IO_2N AB5 uart_tx
+35 EX_IO_1N AB4 uart_rx
+37 GND \
+39 A3V3 \
--- impl.tcl
+++ impl.tcl
@@ -57,3 +57,24 @@ launch_tasks [get_tasks {pnr_1}] -to_action report_timing
 wait_on_tasks [get_tasks {pnr_1}] -to_action report_timing
 launch_tasks [get_tasks {pnr_1}] -to_action gen_bit_stream
 wait_on_tasks [get_tasks {pnr_1}] -to_action gen_bit_stream
+remove_constraint  -logic -fdc "E:/code/PangoWork/Works/cboard_imu/Constraints/io/uart.fdc"
+add_constraint "E:/code/PangoWork/Works/cboard_imu/Constraints/io/cboard_imu.fdc"
+set_option max_threads 0
+set_arch -family Logos -device PGL50H -speedgrade -6 -package FBG484
+set_option -options { top_module {uart_loop_test_top} top_library {work}} [get_filesets design_1]
+launch_tasks [get_tasks {syn_1}] -to_action compile
+wait_on_tasks [get_tasks {syn_1}] -to_action compile
+launch_tasks [get_tasks {syn_1}] -to_action synthesize
+wait_on_tasks [get_tasks {syn_1}] -to_action synthesize
+launch_tasks [get_tasks {pnr_1}] -to_action dev_map
+wait_on_tasks [get_tasks {pnr_1}] -to_action dev_map
+launch_tasks [get_tasks {pnr_1}] -to_action place
+wait_on_tasks [get_tasks {pnr_1}] -to_action place
+launch_tasks [get_tasks {pnr_1}] -to_action route
+wait_on_tasks [get_tasks {pnr_1}] -to_action route
+launch_tasks [get_tasks {pnr_1}] -to_action route_optimize
+wait_on_tasks [get_tasks {pnr_1}] -to_action route_optimize
+launch_tasks [get_tasks {pnr_1}] -to_action report_timing
+wait_on_tasks [get_tasks {pnr_1}] -to_action report_timing
+launch_tasks [get_tasks {pnr_1}] -to_action gen_bit_stream
+wait_on_tasks [get_tasks {pnr_1}] -to_action gen_bit_stream
